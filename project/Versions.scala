6c04c5c6a0b1bf1ef61e171affa114802091bb8f
Tyler Ryan <tyler.j.ryan@jpl.nasa.gov>
2016-02-16 17:35:05 +0000

    Fix version number to use x.x.x format

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,7 +1,7 @@
 object Versions {
 
   // this project
-  val version="1.3"
+  val version="1.3.0"
 
   // https://github.jpl.nasa.gov/mbee-dev/cae.magicdraw.packages.vendor/tree/sp5
   val vendor_package = "1.6"
